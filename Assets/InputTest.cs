4573b5c5242eef3afa3da21d7d764384cfe542ac
kerrot <kerrot.gs@gmail.com>
2018-11-10 18:12:47 +0900

    INIT

---
+++ Assets/InputTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class InputTest : MonoBehaviour {
+
+    public SteamVR_Input_Sources HandType;
+    public SteamVR_Action_Boolean GrabAction;
+    public SteamVR_Action_Vector3 AxisInput;
+
+    [SerializeField]
+    Vector3 axis;
+
+    void Update()
+    {
+        if (GrabAction.GetStateDown(HandType))
+        {
+            Debug.Log("GetStateDown");
+        }
+
+        if (GrabAction.GetStateUp(HandType))
+        {
+            Debug.Log("GetLastStateUp");
+        }
+
+        axis = AxisInput.GetAxis(HandType);
+    }
+}
